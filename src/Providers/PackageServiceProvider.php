475f387d8cd4c3c6af57929afe2e0612ebce972a
thuraaung2493 <thuraaung2493@gmail.com>
2023-07-23 10:14:58 +0630

    class bind for facade

--- src/Providers/PackageServiceProvider.php
+++ src/Providers/PackageServiceProvider.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace Thuraaung\ApiHelpers\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Thuraaung\ApiHelpers\Http\Headers;
 
 final class PackageServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        $this->app->bind(Headers::class, fn () => new Headers());
+
         $this->mergeConfigFrom(
             __DIR__ . '/../../config/api-helpers.php',
             'api-helpers'
